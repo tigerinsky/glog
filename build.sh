33b28bc98f3be435c7cb44bb713017fdb6980465
way <tangdibt@163.com>
2015-04-14 19:46:01 +0800

    init

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+base_dir=`pwd`
+tmp_dir=$base_dir/tmp
+
+
+if [ ! -d $base_dir/../gflags ]
+then
+    echo -e "\033[31m glog: cant't find gflags\033[0m"
+    exit 1
+fi
+
+rm -rf $tmp_dir 
+mkdir -p $tmp_dir
+git clone -b v0.3.3 https://github.com/google/glog.git $tmp_dir  
+if [ $? -ne 0 ]
+then
+    echo -e "\033[31m glog: download from git error \033[0m"
+    exit 1
+fi
+
+cd $tmp_dir
+./configure --prefix=${base_dir} --enable-shared=no --with-gflags=$base_dir/../gflags && make && make install
+if [ $? -ne 0 ]
+then
+    echo -e "\033[31m glog: make error \033[0m"
+    exit 1
+fi
+rm -rf $tmp_dir
+
+
