ad602da1243a0aac7e752c1d68a8fa9c6307aa5c
waliceSnow <wal_ice@163.com>
2015-11-23 14:18:53 +0800

    wangxuebing added -lrt for g++ 4.9.2

--- build.sh
+++ build.sh
@@ -19,6 +19,8 @@ then
     exit 1
 fi
 
+export LDFLAGS="${LDFLAGS} -lrt"
+
 cd $tmp_dir
 ./configure --prefix=${base_dir} --enable-shared=no --with-gflags=$base_dir/../gflags && make && make install
 if [ $? -ne 0 ]
